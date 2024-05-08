7c3a3d53c858272181601a4029d968f17b37859e
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-05-08 22:45:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 365.87 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -2,34 +2,35 @@ from collections import Counter
 
 def solution(topping):
     answer = 0
+    n = len(topping) # 8
     
-    right_counter = dict(Counter(topping))
+    right_counter = dict(Counter(topping)) # 쪼개기 전 전체 topping 을 Counter 객체로 만든 걸로 초기화
     left_counter = {}
     
-    right_unique = len(set(topping))
-    left_unique = 0
+    unique_right = len(right_counter) # 4
+    unique_left = 0
     
     for i in topping:
-        if i not in left_counter:
+        if i not in left_counter: # 왼쪽에 새로운 토핑 등장
             left_counter[i] = 1
-            left_unique += 1
+            unique_left += 1
             right_counter[i] -= 1
             
-            if right_counter[i] == 0:
-                right_unique -= 1
-            
-            if left_unique == right_unique:
+            if right_counter[i] == 0: # 왼쪽에 토핑 할당하느라 오른쪽에 더이상 그 토핑 존재하지 않는다면
+                unique_right -= 1
+                
+            if unique_left == unique_right: # 정답이 되는 경우의 수라면
                 answer += 1
-        
-        else:
+          
+        else: # 왼쪽에게 있어서 새로운 토핑은 아니지만
             left_counter[i] += 1
             right_counter[i] -= 1
             
             if right_counter[i] == 0:
-                right_unique -= 1
+                unique_right -= 1
                 
-            if left_unique == right_unique:
+            if unique_left == unique_right:
                 answer += 1
             
-
+                     
     return answer
