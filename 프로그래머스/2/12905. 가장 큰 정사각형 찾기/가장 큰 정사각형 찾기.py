8657b911207b78e5064149a5538fc0677a42744d
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-05-12 23:37:01 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 674.81 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,18 +1,18 @@
 def solution(board):
-    memoization = board
-    height = len(board)
-    width = len(board[0])
+    answer = 0
+    memo = board
+    height = len(memo)
+    width = len(memo[0])
     maximum = 0
     
-    for i in range(1, height):
-        for j in range(1, width):
-            if memoization[i][j] >= 1 and memoization[i-1][j] >= 1 and memoization[i][j-1] >= 1 and memoization[i-1][j-1] >= 1:
-                memoization[i][j] = min(memoization[i-1][j], memoization[i][j-1], memoization[i-1][j-1]) + 1
-                
-    for row in memoization:
-        for col in row:
-            if col > maximum:
-                maximum = col
+    for row in range(1, height):
+        for col in range(1, width):
+            if memo[row][col] == 1 and memo[row][col-1] >= 1 and memo[row-1][col] >= 1 and memo[row-1][col-1] >= 1:
+                memo[row][col] = min(memo[row][col-1], memo[row-1][col], memo[row-1][col-1]) + 1
 
-    
-    return maximum**2
+    for i in memo:
+        for j in i:
+            if j > maximum:
+                maximum = j
+
+    return maximum ** 2
