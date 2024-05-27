fa949db026f689aaba8ead4a818a9a267e3c221f
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-05-27 16:13:08 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,18 +1,16 @@
-from heapq import heappush, heappop
-
 def solution(number, k):
-    initial_k = k
     
+    initial_k = k
     answer = ''
     n = len(number)
-    hq = []
+    stack = []
 
     for idx, num in enumerate(number): # (0,1), (1,9), (2,2), (3,4)
-        while k and hq and int(hq[-1]) < int(num):
-            hq.pop()
+        while k and stack and int(stack[-1]) < int(num):
+            stack.pop()
             k -= 1
-        hq.append(num)
-    answer = ''.join(hq)
+        stack.append(num)
+    answer = ''.join(stack)
         
     if len(answer) > n - initial_k:
         return answer[:n-initial_k]
