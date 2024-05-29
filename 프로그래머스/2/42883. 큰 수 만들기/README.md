476cb08bf226df9da5db0206c74cb762ce9a90e4
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-05-29 21:42:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 27일 16:13:05
+2024년 05월 29일 21:42:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,21 +1,20 @@
+# from heapq import heappush, heappop
+
 def solution(number, k):
-    
     initial_k = k
+    
     answer = ''
     n = len(number)
-    stack = []
+    hq = []
 
     for idx, num in enumerate(number): # (0,1), (1,9), (2,2), (3,4)
-        while k and stack and int(stack[-1]) < int(num):
-            stack.pop()
+        while k and hq and int(hq[-1]) < int(num):
+            hq.pop()
             k -= 1
-        stack.append(num)
-    answer = ''.join(stack)
+        hq.append(num)
+    answer = ''.join(hq)
         
     if len(answer) > n - initial_k:
         return answer[:n-initial_k]
     
     return answer
-            
-        
-    
