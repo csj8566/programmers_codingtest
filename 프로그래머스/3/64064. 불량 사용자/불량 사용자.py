cb8503e86f8469c85936b93b9c22adc73168a7b6
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-05-31 02:35:24 +0900

    [level 3] Title: 불량 사용자, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -15,17 +15,24 @@ def solution(user_id, banned_id):
     print(lst)
     
     def DFS(depth, path, visited):
-        if depth == len(lst):
-            answer.add(tuple(sorted(path))) #set에는 tuple은 추가 가능하지만 list는 안됨
-            return
-        for user in lst[depth]:
-            if user not in visited:
-                visited.add(user)
-                DFS(depth+1, path + [user], visited)
-                visited.remove(user)
-            
+        ### 루트가 없는 백트래킹 DFS ###
         
+        # 1. visited 에 없으면 visited 에 추가해주고 자식을 돌도록 need_visited에 추가(혹은 그에 준)
+        # 2. 종료조건 세팅 : depth가 최대치일때 
+        # 3. 백트래킹 
+        
+        if depth == len(banned_id):
+            answer.add(tuple(sorted(path)))
+            return
+        for name in lst[depth]:
+            if name not in visited:
+                visited.append(name)
+                DFS(depth+1, path+[name], visited)
+                visited.pop()
+    
     answer = set()
-    DFS(0, [], set())
-    print(lst)
+    DFS(depth=0, path=[], visited=[])
+
+    print()
+    print((answer))
     return len(answer)
