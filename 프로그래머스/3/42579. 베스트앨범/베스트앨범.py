ec56a972c9c2359e92f726a38e8491bdcfe3d56e
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-07-04 22:17:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,25 +1,24 @@
 def solution(genres, plays):
     answer = []
-    genre_plays = {genre : 0 for genre in genres}
-    genre_count = {genre : 2 for genre in genres}
-    song_num = [i for i in range(len(plays))]
+    genre_dict = {}
+    playcount_dict = {}
     
+    for idx, name in enumerate(genres):
+        if name not in genre_dict:
+            genre_dict[name] = 0
+        if name not in playcount_dict:
+            playcount_dict[name] = []
+        genre_dict[name] += plays[idx]
+        playcount_dict[name].append((idx, plays[idx]))
+    # print(genre_dict)
+    # print(playcount_dict)
+    # print()
+    sorted_lst = sorted(list(genre_dict.items()), key=lambda x:x[1], reverse=True)
+    # print(sorted_lst)
     
-    for idx, genre in enumerate(genres): # (0, classic)
-        genre_plays[genre] += plays[idx]
-    
-    
-    zips = list(zip(genres, plays, song_num))
-    zips.sort(key=lambda x:x[1], reverse=True)
-    
-    dicts = list(genre_plays.items())
-    dicts.sort(key=lambda x:x[1], reverse=True)
-    
-    for dic in dicts: # ('pop', 3100)
-        for zp in zips: # ('pop', 2500, 4)
-            if zp[0] == dic[0] and genre_count[zp[0]] > 0:
-                answer.append(zp[2])
-                genre_count[zp[0]] -= 1
-                    
-        
+    for name, playcount in sorted_lst:
+        limit = 0
+        playcount_dict[name].sort(key=lambda x:(-x[1], x[0]))
+        answer.extend([i[0] for i in playcount_dict[name][:2]])
+
     return answer
