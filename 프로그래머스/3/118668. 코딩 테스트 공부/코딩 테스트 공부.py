2488664b7fa425c578376f54d84c74bbaa94738f
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-07-15 17:58:44 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 318.75 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -0,0 +1,37 @@
+def solution(alp, cop, problems):
+    answer = 0
+    max_alp = 0
+    max_cop = 0
+    
+    for row in problems:
+        max_alp = max(max_alp, row[0])
+        max_cop = max(max_cop, row[1])
+        
+    # 최댓값 넘어가는 건 최대값으로 되돌려줘야함
+    # 시간 초과, 세그먼트 폴트와 같은 런타임 에러 방지 위함
+    alp = min(alp, max_alp)
+    cop = min(cop, max_cop)
+            
+    dp = [[999] * (max_cop+1) for _ in range(max_alp+1)]
+    dp[alp][cop] = 0
+    
+    for algo in range(alp, max_alp+1):
+        for coding in range(cop, max_cop+1):
+            # print(algo, coding)
+            if algo < max_alp:
+                dp[algo+1][coding] = min(dp[algo+1][coding], dp[algo][coding] + 1)
+            if coding < max_cop:
+                dp[algo][coding+1] = min(dp[algo][coding+1], dp[algo][coding] + 1)
+            for problem in problems:
+                alp_req, cop_req, alp_rwd, cop_rwd, cost = problem
+                
+                # 문제를 풀 수 있는 상황
+                if algo >= alp_req and coding >= cop_req:
+                    # 최댓값 넘어가는 건 최대값으로 되돌려줘야함
+                    new_algo = min(algo+alp_rwd, max_alp)
+                    new_coding = min(coding+cop_rwd, max_cop)
+                    dp[new_algo][new_coding] = min(dp[algo][coding] + cost, dp[new_algo][new_coding])
+                    
+    
+    return dp[max_alp][max_cop]
+
