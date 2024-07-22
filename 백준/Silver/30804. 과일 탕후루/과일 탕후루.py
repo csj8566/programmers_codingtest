833bd216ae253901a3fea06f0266cf5c55f4f78a
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-07-22 16:47:09 +0900

    [Silver II] Title: 과일 탕후루, Time: 336 ms, Memory: 35760 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -5,17 +5,19 @@ tang = list(map(int, input().split()))
 
 start = 0
 max_length = 0
-current_count = Counter()
+types_dict = Counter()
 
 for end in range(N):
-    current_count[tang[end]] += 1
+    types_dict[tang[end]] += 1
 
-    while len(current_count) > 2:
-        current_count[tang[start]] -= 1
-        if current_count[tang[start]] == 0:
-            del current_count[tang[start]]
+    # 현재 윈도우에 포함된 과일 종류가 2를 초과하면 윈도우를 축소
+    while len(types_dict) > 2:
+        types_dict[tang[start]] -= 1
+        if types_dict[tang[start]] == 0:
+            del types_dict[tang[start]]
         start += 1
-    
+
+    # 최대 길이를 갱신
     max_length = max(max_length, end - start + 1)
 
 print(max_length)
