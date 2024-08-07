6fe01b8422c32f4b5017d264ddae0242cce114e9
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-08-07 15:47:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 3004 ms, Memory: 38824 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,9 +1,13 @@
-N, K = list(map(int, input().split()))
+N, K = list(map(int, input().split())) # 4, 7
 dp = [0 for _ in range(K+1)]
 
 for _ in range(N):
-    W, V = list(map(int, input().split()))
-    for i in range(K, W-1, -1):
-        dp[i] = max(dp[i], dp[i-W] + V)
+    W, V = list(map(int, input().split())) # 6, 13 / 4, 8 / 3, 6 / 5, 12
+    new_dp = dp[:]  # 기존 dp 배열을 복사하여 새로운 배열 생성
+
+    for i in range(W, K+1):
+        new_dp[i] = max(dp[i], dp[i-W] + V)
+
+    dp = new_dp  # 새로운 dp 배열로 갱신
 
 print(max(dp))
