81869ab0fe8992a9d3bd0b4b946341b8e70b6a50
csj8566 <113168893+csj8566@users.noreply.github.com>
2024-08-19 23:03:20 +0900

    [Silver I] Title: 문자열 화폐, Time: 2696 ms, Memory: 122868 KB -BaekjoonHub

--- 백준/Silver/17828. 문자열 화폐/문자열 화폐.py
+++ 백준/Silver/17828. 문자열 화폐/문자열 화폐.py
@@ -1,28 +1,31 @@
 N, X = list(map(int, input().split()))
 
 lst = [1] * N
-total = N  # 초기값은 모두 1로 채워져 있으므로 total은 N과 같음
+total = 1 * N
 alp = []
 
 def num_alp(num):
     if 1 <= num <= 26:
         return chr(num + 64)
 
-if X < N or X > 26 * N:
+
+if (X < N) or X > (26 * N):
     print('!')
+
 else:
-    for i in range(N):
-        remaining = X - total  # 남은 값을 계산
+    for i in range(N): # 0~5 (총 6번)
+        remaining = X - total
         if remaining <= 0:
             break
-        
-        # 현재 위치에서 더할 수 있는 최대 값은 25(현재 1이 들어가 있으므로)
+
         add_value = min(25, remaining)
+        total += add_value
         lst[i] += add_value
-        total += add_value  # total을 갱신
-    
+        
+
+    # print(lst)
     for letter in lst:
         alp.append(num_alp(letter))
-    
+
     alp.sort()
     print(''.join(alp))
