33fa17f0dcc5c96728bd702d4a0f281d4519e211
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-03-19 09:22:50 +0900

    [level 2] Title: 귤 고르기, Time: 93.03 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,36 @@
-from collections import Counter
+from collections import Counter, deque 
 
 def solution(k, tangerine):
-    tang_dict = Counter(tangerine)
-    tang_length = []
+    answer = 0
+    tan_counter = Counter(tangerine)
+    # print(tan_counter)
     
-    for i in tang_dict.values():
-        tang_length.append(i)
-        
-    tang_length = sorted(tang_length, reverse = True)
+    sorted_counter = tan_counter.items()
+    # print(sorted_counter)
     
-    cnt = 0
-    for idx, num in enumerate(tang_length, start = 1):
-        cnt += num
-        if cnt >= k:
-            return idx
+    sorted_counter = sorted(sorted_counter, key = lambda x : x[1], reverse=True)
+    # print(sorted_counter)
+    
+    count = 0
+    temp_list = []
+    queue = deque(sorted_counter)
+    while k > count:
+        if not queue:
+            continue
+        tan, num = queue.popleft()
+        for _ in range(num):
+            temp_list.append(tan)
+            count += 1
+            
+    temp_set = set(temp_list)
+    
+    return len(temp_set)
+
+
+'''
+한 박스에 들어가는 귤의 종류가 "최소"가 되게끔 -> 그리디
+
+한 종류의 귤을 담는다 했을 때 최대한 그 같은 크기는 다 담는 게 좋음
+그렇다면 정렬을 하는 게 좋겠네? key 는 개수로!
+
+'''
