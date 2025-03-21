0483729f783ec3d081631891c80cebc85f5fed54
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-03-21 11:51:07 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 345.14 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,31 +1,17 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
     
-    remain_del = 0
-    remain_pick = 0
+    d_remain = 0 # 배달할 거 얼마나 남았는지
+    p_remain = 0 # 반납할 거 얼마나 남았는지
     
-    # 뒤에서부터 시작해야 최소한의 거리로 왔다갔다 할 수 있음
-    # 만약 앞에서부터 고려한다면 쓸데없는 반복이 추가됨
-    
-    # deliveries와 pickups 배열을 반대로 뒤집으셈
-    deliveries.reverse()
-    pickups.reverse()
-    
-    # 이제 집 하나씩 돌 건데
-    for i in range(n):
+    for i in range(n-1, -1, -1): # 맨 뒤에서 처음까지
+        d_remain += deliveries[i] # 현재 집에서는 배달할 거 얼마나 있는지 더해주기
+        p_remain += pickups[i] # 현재 집에서는 반납할 거 얼마나 있는지 더해주기
         
-        # 한 집 당 해결해야하는 남은배달량, 수거량
-        remain_del += deliveries[i]
-        remain_pick += pickups[i] 
-       
-        # remain_del 이나 remain_pick 둘 중 하나라도 0 초과인 경우 계속 왔다갔다 해야함
-        while (remain_del > 0) or (remain_pick > 0):
-            
-            # 트럭에는 최대 cap만큼 택배를 실어서 출발할 수 있음. 
-            # remain_del 과 remain_pick은 음수여도 괜찮음. 음수인 동안은 answer에 거리안더함
-            # 변수가 음수인 동안, i번째 집까지 오는 동안의 택배 업무를 한번에 모두 처리할수있단뜻 
-            remain_del -= cap # 2
-            remain_pick -= cap # 3
-            answer += (n-i) * 2
+        while (d_remain > 0) or (p_remain > 0): # 만약 배달할 거 / 반납할 거 -> 하나라도 남았다면
+            d_remain -= cap
+            p_remain -= cap
+            answer += ((i + 1) * 2)
+        
     
     return answer
