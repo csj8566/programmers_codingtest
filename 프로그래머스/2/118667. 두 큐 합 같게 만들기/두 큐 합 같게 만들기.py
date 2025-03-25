e9d55884e67d86eb76c4c4761d37483f16836bce
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-03-25 09:09:50 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 78.46 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -3,29 +3,37 @@ from collections import deque
 def solution(queue1, queue2):
     deque1 = deque(queue1)
     deque2 = deque(queue2)
-    total_deque = deque1 + deque2
-    sum_1 = sum(deque1)
-    sum_2 = sum(deque2)
     
-    limit = len(total_deque) * 2
-    cnt = 0
-    trial = 0
+    limit = (len(deque1) + len(deque2)) * 2
+    sum1 = sum(deque1)
+    sum2 = sum(deque2)
+    
+    count = 0
+    
     while True:
-        if sum_1 < sum_2:
-            target = deque2.popleft()
-            deque1.append(target)
-            cnt += 1
-            sum_1 += target
-            sum_2 -= target
-            trial += 1
-        elif sum_1 > sum_2:
-            target = deque1.popleft()
-            deque2.append(target)
-            cnt += 1
-            sum_1 -= target
-            sum_2 += target
-            trial += 1
+        if sum1 < sum2:
+            temp = deque2.popleft()
+            deque1.append(temp)
+            
+            # sum() 연산을 다시 하는 건 시간복잡도 손해
+            sum2 -= temp
+            sum1 += temp
+            
+            count += 1
+            
+        elif sum1 > sum2:
+            temp = deque1.popleft()
+            deque2.append(temp)
+            
+            sum1 -= temp
+            sum2 += temp
+            
+            count += 1
+            
         else:
-            return cnt
-        if trial >= limit:
+            return count
+        
+        # 만약 두 deque 를 합친 길이만큼 넣었다 뺐다를 반복했는데도 안된다면
+        # 그건 불가능한 경우
+        if count >= limit:
             return -1
