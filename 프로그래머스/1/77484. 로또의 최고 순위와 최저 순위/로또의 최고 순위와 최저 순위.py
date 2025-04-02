899fef394ea92e22330e2916933e9af02a591070
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-04-02 09:29:56 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,53 @@
+def solution(lottos, win_nums):
+    answer = []
+    
+    win_dict = {6 : 1,
+                5 : 2,
+                4 : 3,
+                3 : 4,
+                2 : 5,
+                1 : 6,
+                0 : 6}
+    
+    # 일단 초기 상태에서 lottos 와 win_nums 에 일치하는 게 몇 개 존재하는지 확인
+    same_count = 0
+    zero_count = 0
+    for l in lottos:
+        for w in win_nums:
+            # print("현재 l :",l)
+            # print("현재 w :", w)
+            # print()
+            if l == 0:
+                zero_count += 1
+                # print("zero_count 추가! :", zero_count)
+                # print()
+                break
+                
+            if l == w:
+                same_count += 1
+                # print("same_count 추가! :", same_count)
+                # print()
+
+                
+    # 최대는 0 개수 만큼 더해서
+    answer.append(win_dict[same_count + zero_count])
+    # print("최대 ---------------------")
+    # print("same_count :", same_count)
+    # print("zero_count :", zero_count)
+    # print()
+    
+    # 최소는 현재 상태 그대로
+    answer.append(win_dict[same_count])
+    # print("최소 ---------------")
+    # print("same_count :", same_count)
+    # print("zero_count :", zero_count)
+    
+    
+    return answer
+
+
+
+'''
+둘 다 길이 엄청 짧으니까 그냥 개지랄하면서 탐색해도 됨
+
+'''
