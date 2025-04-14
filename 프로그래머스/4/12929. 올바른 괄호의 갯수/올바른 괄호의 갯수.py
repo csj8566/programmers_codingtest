a72038c4a5b3ba5a80497b87fbf7380ea288c65f
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-04-14 10:25:47 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 2880.20 ms, Memory: 239 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,17 +1,24 @@
 def solution(n):
     answer = []
     
-    def DFS(current_state, num_open, num_close):
-        if num_open == n and num_close == n:
+    def DFS(current_state, num_open, num_closed):
+        
+        # 종료 조건 : 열린 거 n개, 닫힌 거 n개 정상적으로 다 썼을 때
+        if num_open == n and num_closed == n:
             answer.append(current_state)
             return
         
+        # 만약 열린 괄호 개수보다 닫힌 괄호 개수가 더 많다면 그건 안됨
+        # 만약 열린 괄호가 더 많은 경우에는 그 다음에는 열린 괄호든, 닫힌 괄호든 올 수 있음
+        # 근데 열린 괄호는 최대 n개까지밖에 올 수 없음
         if num_open < n:
-            DFS(current_state + '(', num_open + 1, num_close)
+            DFS(current_state + '(', num_open + 1, num_closed)
             
-        if num_close < num_open:
-            DFS(current_state + ')', num_open, num_close + 1)
+        if num_closed < num_open:
+            DFS(current_state + ')', num_open, num_closed + 1)
             
+    
+    # 모든 탐색을 다 끝내구 가능한 경우의 수를 return 하면 그게 정답
     DFS('', 0, 0)
     
     
