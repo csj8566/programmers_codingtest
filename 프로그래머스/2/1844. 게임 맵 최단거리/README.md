e83394a608e81776aec2eea48bbb1e743e9c1719
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-05-05 15:23:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.05 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.46 MB, 시간: 9.41 ms
+메모리: 9.37 MB, 시간: 10.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 03일 00:51:31
+2025년 05월 05일 15:23:27
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,35 +3,35 @@ from collections import deque
 def solution(maps):
     answer = 0
     moves = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상 하 좌 우
-    num_rows = len(maps)
-    num_cols = len(maps[0])
+    num_row = len(maps)
+    num_col = len(maps[0])
     
-    visited = [[False] * num_cols for _ in range(num_rows)]
-    # print(visited)
-    
-    def BFS(visited):
+    def BFS():
+        visited = [[False] * num_col for _ in range(num_row)]
         queue = deque()
-        
         visited[0][0] = True
-        queue.append((0, 0, 1))
-    
+        queue.append((0, 0, 1)) 
+        
         while queue:
-            current_row, current_col, current_cost = queue.popleft()
-            
+            current_row, current_col, cost = queue.popleft()
             # 종료 조건
-            if current_row == num_rows - 1 and current_col == num_cols - 1:
-                return current_cost
+            if current_row == (num_row - 1) and current_col == (num_col - 1):
+                return cost
             
             for row_move, col_move in moves:
                 next_row = current_row + row_move
                 next_col = current_col + col_move
                 
-                if 0<=next_row<num_rows and 0<=next_col<num_cols:
-                    if maps[next_row][next_col] == 1 and not visited[next_row][next_col]: # 벽으로 막혀있지 않음
+                # 갈 수 있는 칸인 경우
+                if 0<=next_row<num_row and 0<=next_col<num_col and maps[next_row][next_col] == 1:
+                    
+                    # visited 절대 까먹지마!
+                    if visited[next_row][next_col] == False:
                         visited[next_row][next_col] = True
-                        queue.append((next_row, next_col, current_cost + 1))
+                        queue.append((next_row, next_col, cost + 1))
                         
         return -1
-        
-        
-    return BFS(visited)
+                    
+            
+    
+    return BFS()
