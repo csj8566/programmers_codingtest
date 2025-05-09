3c520c0c5c1948dcbc1c0ee1d47239b173005d3f
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-05-09 17:00:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.89 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,36 +2,34 @@ from collections import deque
 
 def solution(maps):
     answer = 0
-    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상 하 좌 우
-    num_row = len(maps)
-    num_col = len(maps[0])
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    num_rows = len(maps)
+    num_cols = len(maps[0])
     
     def BFS():
-        visited = [[False] * num_col for _ in range(num_row)]
+        visited = [[False] * num_cols for _ in range(num_rows)]
         queue = deque()
         visited[0][0] = True
-        queue.append((0, 0, 1)) 
+        queue.append((0, 0, 1))
         
         while queue:
-            current_row, current_col, cost = queue.popleft()
+            current_row, current_col, count = queue.popleft()
+            
             # 종료 조건
-            if current_row == (num_row - 1) and current_col == (num_col - 1):
-                return cost
+            if current_row == num_rows - 1 and current_col == num_cols - 1:
+                return count
             
             for row_move, col_move in moves:
                 next_row = current_row + row_move
                 next_col = current_col + col_move
                 
-                # 갈 수 있는 칸인 경우
-                if 0<=next_row<num_row and 0<=next_col<num_col and maps[next_row][next_col] == 1:
-                    
-                    # visited 절대 까먹지마!
+                if 0<=next_row<num_rows and 0<=next_col<num_cols and maps[next_row][next_col] == 1:
                     if visited[next_row][next_col] == False:
                         visited[next_row][next_col] = True
-                        queue.append((next_row, next_col, cost + 1))
+                        queue.append((next_row, next_col, count+1))
                         
         return -1
-                    
-            
+        
+    
     
     return BFS()
