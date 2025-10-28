7c9f38af0a8f3755638b77010c2457c67fb02b7e
SUHYEON KO <113168893+csj8566@users.noreply.github.com>
2025-10-28 23:15:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.29 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,34 +2,29 @@ from collections import deque
 
 def solution(maps):
     answer = 0
-    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     num_rows = len(maps)
     num_cols = len(maps[0])
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
-    def BFS():
-        visited = [[False] * num_cols for _ in range(num_rows)]
-        queue = deque()
-        visited[0][0] = True
-        queue.append((0, 0, 1))
+    visited = [([False] * num_cols) for _ in range(num_rows)]
+    visited[0][0] = True
+    queue = deque([(0, 0, 1)])
+    
+    while queue:
+        current_row, current_col, count = queue.popleft()
         
-        while queue:
-            current_row, current_col, count = queue.popleft()
+        # 종료 조건
+        if current_row == num_rows - 1 and current_col == num_cols - 1:
+            return count
+        
+        for row_move, col_move in moves:
+            next_row = current_row + row_move
+            next_col = current_col + col_move
             
-            # 종료 조건
-            if current_row == num_rows - 1 and current_col == num_cols - 1:
-                return count
-            
-            for row_move, col_move in moves:
-                next_row = current_row + row_move
-                next_col = current_col + col_move
+            # 다음 칸이 갈 수 있는 곳이라면
+            if 0 <= next_row < num_rows and 0 <= next_col < num_cols and maps[next_row][next_col] == 1 and visited[next_row][next_col] == False:
+                visited[next_row][next_col] = True
+                queue.append([next_row, next_col, count + 1])
                 
-                if 0<=next_row<num_rows and 0<=next_col<num_cols and maps[next_row][next_col] == 1:
-                    if visited[next_row][next_col] == False:
-                        visited[next_row][next_col] = True
-                        queue.append((next_row, next_col, count+1))
-                        
-        return -1
-        
-    
-    
-    return BFS()
+            
+    return -1
